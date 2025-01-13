308f3957502ae9b955c2b1776e6b54b6c4771280
spotcircuit <info@spotcircuit.com>
2025-01-13 13:07:17 -0500

    refactor: Remove Results component from index page

---
+++ SpotCircuit/ecommerce-offers/shopify_offer/pages/index.tsx
@@ -0,0 +1,25 @@
+import Hero from '../components/Hero';
+import Problem from '../components/Problem';
+import Services from '../components/Services';
+import FAQ from '../components/FAQ';
+import Booking from '../components/Booking';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <Header />
+      <main>
+        <div className="relative">
+          <Hero />
+          <Problem />
+        </div>
+        <Services />
+        <FAQ />
+        <Booking />
+      </main>
+      <Footer />
+    </div>
+  );
+}
